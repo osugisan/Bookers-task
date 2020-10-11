c233464607449b995559f68b37285fb211a9b55e
EC2 Default User <ec2-user@ip-172-31-79-239.ec2.internal>
2020-10-11 06:34:43 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, except: [:new]
-  get 'books' => 'books#new'
   root 'books#top'
 
   end
